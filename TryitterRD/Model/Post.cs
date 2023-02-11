102a388acb39c15847533e562b7854f1b467dca7
Diogo <diogompantaleao@gmail.com>
2023-02-10 23:43:26 -0300

    feat: models for user and post created and contextDB defined

---
+++ TryitterRD/Model/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TryitterRD.Model
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Title { get; set; } = default!;
+        public string Content { get; set; } = default!;
+
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+        public virtual User User { get; set; } = default!;
+    }
+}
---
+++ TryitterRD/Model/TryitterContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TryitterRD.Model
+{
+    public class TryitterContext : DbContext
+    {
+        public TryitterContext(DbContextOptions<TryitterContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
