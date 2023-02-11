bb6680d9292a565ba191cbdcc5e7170e50d07ad5
Diogo <diogompantaleao@gmail.com>
2023-02-11 01:08:29 -0300

    chore: we setup program to check connection string default, bilder services and authentication check

--- TryitterRD/Program.cs
+++ TryitterRD/Program.cs
@@ -1,3 +1,12 @@
+using TryitterRD;
+using TryitterRD.Model;
+using TryitterRD.Repository;
+using TryitterRD.Repository.Implementation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,30 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<TryitterContext>(option => option.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+var cryptKey = Encoding.ASCII.GetBytes(JWTSecretKey.JwtSecretKey);
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(auth =>
+{
+    auth.RequireHttpsMetadata = false;
+    auth.SaveToken = true;
+    auth.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(cryptKey),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+});
 
 var app = builder.Build();
 
