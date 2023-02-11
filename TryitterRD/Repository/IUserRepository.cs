7f55741f380cbe2f8167bdbea66ed2228565c3e4
Diogo <diogompantaleao@gmail.com>
2023-02-10 23:44:44 -0300

    feat: User repository with interface created

---
+++ TryitterRD/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using TryitterRD.Model;
+
+namespace TryitterRD.Repository
+{
+    public interface IUserRepository
+    {
+        public void Save(User user);
+        public bool CheckByEmail(string email);
+        User GetUserByLoginDTO(string email, object password);
+        User GetUserById(int id);
+    }
+
+}
---
+++ TryitterRD/Repository/Implementation/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using TryitterRD.Model;
+
+namespace TryitterRD.Repository.Implementation
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly TryitterContext _context;
+        public UserRepository(TryitterContext context)
+        {
+            _context = context;
+        }
+
+        public void Save(User user)
+        {
+            _context.Add(user);
+            _context.SaveChanges();
+        }
+
+        public bool CheckByEmail(string email)
+        {
+            return _context.Users.Any(user => user.Email.ToLower() == email.ToLower());
+        }
+
+        public User GetUserByLoginDTO(string email, object password)
+        {
+            return _context.Users.FirstOrDefault(user => user.Email == email && user.Password == password);
+        }
+
+        public User GetUserById(int id)
+        {
+            return _context.Users.FirstOrDefault(user => user.UserId == id);
+        }
+    }
+}
